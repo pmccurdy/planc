3ac8306a2df4134748c2741741a749b747e4d347
Peter McCurdy <pmccurdy@navarra.ca>
2010-11-27 13:51:54 -0500

    Add some movie details.

--- movie.php
+++ movie.php
@@ -1,4 +1,26 @@
 <?php
     $movie_url = $_GET['movie'];
-    echo("Details for $movie_url");
+
+    $handle = fopen($movie_url . ".json", "r");
+    $movie_json = stream_get_contents($handle);
+    fclose($handle);
+
+    $movie = json_decode($movie_json, TRUE);
+    echo("<h3>Details for " . $movie['title'] . "</h3>");
+
+    if (array_key_exists('posters', $movie)) {
+        foreach ($movie['posters'] as $poster) {
+            if ($poster['size'] == 'large')
+                echo("<img src=" . $poster['href'] . ">");
+        }
+    }
+
+    echo("<table><tr><th>Theatre</th><th>Showtimes</th></tr>");
+    foreach ($movie['theaters'] as $theater) {
+        $times = implode(' ', $theater['times']);
+        echo("<tr><td>" . $theater['name'] . "</td>");
+        echo("<td>Today: $times</td>");
+        echo("</tr>");
+    }
+    echo("</table>");
 ?>
