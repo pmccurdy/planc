0c5aa680575e0340368942d2e8d5047b87657511
Peter McCurdy <pmccurdy@navarra.ca>
2010-11-27 16:03:19 -0500

    Add links for expressing/unexpressing interest.

--- movie.php
+++ movie.php
@@ -1,5 +1,43 @@
 <?php
+    function express_interest($userid, $movie_url, $interested)
+    {
+        if (isset($userid)) {
+            $rs = mysql_query("SELECT ml_id FROM plancDB.movie_list WHERE " .
+                "pp_id = " . intval($userid) . " " .
+                "AND ml_movie = '" . mysql_real_escape_string($movie_url) . "'");
+
+            if (!$rs) {
+                $message  = 'Invalid query: ' . mysql_error() . "\n";
+                $message .= 'Whole query: ' . $query;
+                die($message);
+            }
+
+            $row = mysql_fetch_assoc($rs);
+            if ($row && !$interested) {
+                $rs = mysql_query("DELETE FROM plancDB.movie_list WHERE " .
+                    "ml_id = " . $row['ml_id']);
+                if (!$rs) {
+                    $message  = 'Invalid query: ' . mysql_error() . "\n";
+                    $message .= 'Whole query: ' . $query;
+                    die($message);
+                }
+            }
+            if (!$row && $interested) {
+                $movie_escaped = mysql_real_escape_string($movie_url);
+                $rs = mysql_query("INSERT INTO plancDB.movie_list VALUES (0, $userid, '$movie_escaped')");
+                if (!$rs) {
+                    $message  = 'Invalid query: ' . mysql_error() . "\n";
+                    $message .= 'Whole query: ' . $query;
+                    die($message);
+                }
+            }
+        }
+    }
+
     $movie_url = $_GET['movie'];
+
+    if (array_key_exists('interest', $_GET))
+        express_interest($_SESSION['user'], $movie_url, $_GET['interest']);
 
     $handle = fopen($movie_url . ".json", "r");
     $movie_json = stream_get_contents($handle);
@@ -41,7 +79,9 @@
             die($message);
         }
 
+        $num_friends = 0;
         while ($row = mysql_fetch_assoc($rs)) {
+            $num_friends++;
             $friend_id = $row['pp_id'];
 
             $friend_rs = mysql_query("SELECT pp_name, pp_img FROM plancDB.people " .
@@ -56,6 +96,28 @@
             $friend = mysql_fetch_assoc($friend_rs);
             echo(" <img width=48 height=48 src=images/" . $friend['pp_img'] . ">");
             echo($friend['pp_name']);
+        }
+
+        if ($num_friends == 0) {
+            echo("No friends interested in this movie<br>");
+        }
+
+        $rs = mysql_query("SELECT COUNT(pp_id) FROM plancDB.movie_list " .
+            "WHERE pp_id = $user " . 
+            "AND ml_movie = '$movieid' ");
+
+        if (!$rs) {
+            $message  = 'Invalid query: ' . mysql_error() . "\n";
+            $message .= 'Whole query: ' . $query;
+            die($message);
+        }
+
+        $row = mysql_fetch_array($rs);
+        $i_am_interested = $row[0] > 0;
+        if ($i_am_interested) {
+            echo("<br><br><a href='index.php?current=movie&movie=$movie_url&interest=0'>I don't want to go any more</a>");
+        } else {
+            echo("<br><br><a href='index.php?current=movie&movie=$movie_url&interest=1'>Sounds fun!</a>");
         }
     }
 ?>
