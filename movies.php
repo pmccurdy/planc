473ce7e4d9dca601438b423d6748d3e87f37e9b9
Peter McCurdy <pmccurdy@navarra.ca>
2010-11-27 13:24:27 -0500

    Add movies view.

--- movies.php
+++ movies.php
@@ -1,3 +1,14 @@
 <?php
-    echo("Movies");
+    echo("<h3>Movies</h3>");
+
+    $handle = fopen("http://api.cineti.ca/movies.json", "r");
+    $movies_json = stream_get_contents($handle);
+    fclose($handle);
+
+    $movies = json_decode($movies_json, TRUE);
+    foreach ($movies as $movie) {
+        if (array_key_exists('thumbnail', $movie)) {
+            echo("<img src=" . $movie['thumbnail'] . ">");
+        }
+    }
 ?>
