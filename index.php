729d08a8d51c124863228bb93046338de902ec75
Mayous <antoine@wherecloud.com>
2010-11-27 11:46:41 -0500

    index.php and CSS

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">
+
+
+<html>
+<head>
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div id="page">
+	<div id="header"></div>
+
+	<div id="content">
+
+	
+	<?php
+
+		echo 'Bonjour';
+
+	?>
+
+	</div>
+
+	
+	<div id="footer"></div>
+</div>
+
+</body>
+</html>
